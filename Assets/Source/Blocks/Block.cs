ae5406f887e60e8a8479124da9426235b8f13319
LaFriska <harold.gao205@gmail.com>
2022-12-22 23:38:44 +1300

    Block management

--- Assets/Source/Blocks/Block.cs
+++ Assets/Source/Blocks/Block.cs
@@ -1,17 +1,36 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 namespace Source.Blocks
 {
     public class Block
     {
 
-        public int blockID;
-        
+        public GameObject parent;
+
+        private int blockID;
+        private Sprite sprite;
+
         public Block(int blockID, Sprite sprite)
         {
             this.blockID = blockID;
+            this.sprite = sprite;
+            parent = new GameObject(sprite.name + "_parent");
         }
-        
+
+        public void SetBlock(float x, float y)
+        {
+            GameObject newTile = new GameObject("tile");
+            newTile.transform.parent = parent.transform;
+            //newTile.transform.localScale = new Vector3(6.25F, 6.25F, 6.25F);
+            newTile.AddComponent<SpriteRenderer>();
+            newTile.GetComponent<SpriteRenderer>().sprite = sprite;
+            newTile.AddComponent<Variables>();
+            newTile.GetComponent<Variables>().declarations.Set("blockID", blockID);
+            newTile.name = sprite.name;
+            newTile.transform.position = new Vector2(x, y);
+        }
+
         //Getters
 
         public int getBlockID()
--- Assets/Source/TerrainGen.cs
+++ Assets/Source/TerrainGen.cs
@@ -1,86 +1,91 @@
 using System;
+using Source.Blocks;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class TerrainGen : MonoBehaviour
+namespace Source
 {
-    
-    public Sprite desolateRock;
-    public Sprite desolateDirt;
-    public int desolateDirtHeight = 5;
-    
-    //Size of the world.
-    public int horizontalWorldSize = 100;
-    public int verticalWorldSize = 100;
-    //Noise frequency changes how frequent hills are generated.
-    public float terrainFreq = 0.07F;
-    //Height multiplier determines how tall hills are.
-    public float heightMultiplier = 4f;
-    //How deep the world is.
-    public int heightAddition = 25;
-    //Seed
-    public float seed = 0;
-    
-    //Caves
-    public float caveFreq = 0.5F;
-    public float caveSize = 0.7F;
-    public int caveDepth = 20;
-    
-    
-    
-    public Texture2D noiseTexture;
-
-    public void Start()
+    public class TerrainGen : MonoBehaviour
     {
-        seed = UnityEngine.Random.Range(-10000, 10000);
-        GenerateNoiseTexture();
-        GenerateTerrain();
-    }
+        public Sprite desolateRock;
+        public Sprite desolateDirt;
+        public int desolateDirtHeight = 5;
 
-    public void GenerateTerrain()
-    {
-        for (int x = 0; x <= horizontalWorldSize - 1; x++)
+        public Block DesolateRockBlock;
+        public Block DesolateDirtBlock;
+
+        //Size of the world.
+        public int horizontalWorldSize = 100;
+
+        public int verticalWorldSize = 100;
+
+        //Noise frequency changes how frequent hills are generated.
+        public float terrainFreq = 0.07F;
+
+        //Height multiplier determines how tall hills are.
+        public float heightMultiplier = 4f;
+
+        //How deep the world is.
+        public int heightAddition = 25;
+
+        //Seed
+        public float seed = 0;
+
+        //Caves
+        public float caveFreq = 0.5F;
+        public float caveSize = 0.7F;
+        public int caveDepth = 20;
+
+
+        public Texture2D noiseTexture;
+
+        public void Start()
         {
-            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+            seed = UnityEngine.Random.Range(-10000, 10000);
 
-            for (int y = 0; y <= height - 1; y++)
+            DesolateRockBlock = new Block(1, desolateRock);
+            DesolateDirtBlock = new Block(2, desolateDirt);
+
+            GenerateNoiseTexture();
+            GenerateTerrain();
+        }
+
+        public void GenerateTerrain()
+        {
+            for (int x = 0; x <= horizontalWorldSize - 1; x++)
             {
-                if (y < height - desolateDirtHeight)
+                float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier +
+                               heightAddition;
+
+                for (int y = 0; y <= height - 1; y++)
                 {
-                    if (noiseTexture.GetPixel(x, y).r > caveSize || y >= height - caveDepth) GenerateTile(x,y, desolateRock);
-                }
-                else
-                {
-                    GenerateTile(x,y, desolateDirt);
+                    if (y < height - desolateDirtHeight)
+                    {
+                        if (noiseTexture.GetPixel(x, y).r > caveSize || y >= height - caveDepth)
+                            DesolateRockBlock.SetBlock(x, y);
+                    }
+                    else
+                    {
+                        DesolateDirtBlock.SetBlock(x, y);
+                    }
                 }
             }
         }
-    }
 
-    private void GenerateTile(int x, int y, Sprite sprite)
-    {
-        GameObject newTile = new GameObject(name = "tile");
-        newTile.transform.parent = this.transform;
-        //newTile.transform.localScale = new Vector3(6.25F, 6.25F, 6.25F);
-        newTile.AddComponent<SpriteRenderer>();
-        newTile.GetComponent<SpriteRenderer>().sprite = sprite;
-        newTile.name = sprite.name;
-        newTile.transform.position = new Vector2(x + 0.5F, y + 0.5F);
-    }
-
-    public void GenerateNoiseTexture()
-    {
-        noiseTexture = new Texture2D(horizontalWorldSize, verticalWorldSize);
-
-        for (int x = 0; x <= noiseTexture.width - 1; x++)
+        public void GenerateNoiseTexture()
         {
-            for (int y = 0; y <= noiseTexture.height - 1; y++)
+            noiseTexture = new Texture2D(horizontalWorldSize, verticalWorldSize);
+
+            for (int x = 0; x <= noiseTexture.width - 1; x++)
             {
-                float v = Mathf.PerlinNoise((x + seed) * caveFreq, (y + seed) * terrainFreq);
-                noiseTexture.SetPixel(x,y,new Color(v, v, v));
+                for (int y = 0; y <= noiseTexture.height - 1; y++)
+                {
+                    float v = Mathf.PerlinNoise((x + seed) * caveFreq, (y + seed) * terrainFreq);
+                    noiseTexture.SetPixel(x, y, new Color(v, v, v));
+                }
             }
+
+            noiseTexture.Apply();
         }
-        
-        noiseTexture.Apply();
     }
 }
